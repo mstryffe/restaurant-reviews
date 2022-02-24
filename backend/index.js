6580cc02e9bbfab9277738561a87bc4aa0682cd2
mstryffe <mstryff@gmail.com>
2022-02-23 21:26:12 -0500

    Implemented restaurant retrieval by search terms and started work on review submission and deletion routing.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
 dotenv.config()
 
 const MongoClient = mongodb.MongoClient
@@ -16,10 +17,10 @@ MongoClient.connect(
 	)
 	.catch(err => {
 		console.error(err.stack)
-		console.log('error')
 		process.exit(1)
 	})
 	.then(async client => {
+		await RestaurantsDAO.injectDB(client)
 		app.listen(port, () => {
 			console.log('listening on port ' +port)
 		})
